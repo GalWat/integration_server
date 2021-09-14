31eee67f0cb2461c6b14a939ca4acb07d4c302eb
Galeo Watts <watts-galeo@ya.ru>
2021-09-14 20:24:41 +0300

    Initial commit

---
+++ server/flask_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, json
+import git
+
+from statuses_worker import Statuses, StatusesWorker
+
+app = Flask(__name__)
+
+statuses_worker = StatusesWorker()
+
+
+@app.route('/')
+def processing():
+    return "OK"
+
+
+@app.route('/status', methods=['POST'])
+def get_status():
+    data = request.json
+    return statuses_worker.get_status(data['person'])
+
+
+@app.route('/change-status', methods=['POST'])
+def change_status():
+    data = request.json
+    return statuses_worker.change_status(data['person'], data['status'])
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/pythonevenings/repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
---
+++ server/statuses_worker.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class Statuses(Enum):
+    DEFAULT = "Не занят"
+    BUSY = "Занят"
+    READY_FOR_TALK = "Готов общаться"
+    ERROR = "Ошибка"
+
+
+class StatusesWorker:
+    def __init__(self):
+        self.statuses = {
+            'misha': Statuses.DEFAULT,
+            'sveta': Statuses.DEFAULT
+        }
+
+    def get_status(self, person):
+        return self.statuses.get(person, Statuses.ERROR)
+
+    def change_status(self, person, status):
+        self.statuses[person] = status
